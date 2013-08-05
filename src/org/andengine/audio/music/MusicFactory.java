e9c6e987fe61bd01729a845802355d31b0c854fd
Ivan <ivan.bobrov@flexymind.ru>
2013-08-05 15:29:04 +0400

    deleted line 91 in org.andengine.audio.music.MusicFactory

--- src/org/andengine/audio/music/MusicFactory.java
+++ src/org/andengine/audio/music/MusicFactory.java
@@ -88,7 +88,6 @@ public final class MusicFactory {
 
 		final AssetFileDescriptor assetFileDescritor = pContext.getAssets().openFd(MusicFactory.sAssetBasePath + pAssetPath);
 		mediaPlayer.setDataSource(assetFileDescritor.getFileDescriptor(), assetFileDescritor.getStartOffset(), assetFileDescritor.getLength());
-		mediaPlayer.prepare();
 
 		final Music music = new Music(pMusicManager, mediaPlayer);
 		pMusicManager.add(music);
